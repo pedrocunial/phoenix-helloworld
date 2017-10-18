7c47a8a7c67c7498b4d651ab90e07b9e104d2554
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-10-18 21:55:42 -0200

    added dependencies for code quality

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule Hello.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:dogma, "~> 0.1", only: [:dev]},
     ]
   end
 
